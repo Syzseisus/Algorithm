e51d866f34bce6df226be88c24620a4a1acc9666
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-02-20 21:55:33 +0900

    Time: 356 ms (72.84%) | Memory: 22.2 MB (95.47%) - LeetSync

--- LeetSync/1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ LeetSync/1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,28 +1,20 @@
+'''
+score = values[i] + values[j] + i - j
+인데 이걸 다르게 표현하면
+score = (values[i] + i) + (values[j] - j) = rewarded + penalted
+가 된다. 이때 i < j이므로
+정답은 특정 시점의 최대 rewarded (max_rewarded)에
+가능한 penalted를 더한 것 중 최대가 된다
+'''
+
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-#         n = len(values)
-#         if n == 2:
-#             return sum(values) - 1
-
-#         answer = 0
-#         max_rewarded = values[0] + 0
-#         for i in range(1, n):
-#             v = values[i]
-#             penalted = v - i
-#             answer = max(answer, max_rewarded + penalted)
-#             rewarded = v + i
-#             max_rewarded = max(max_rewarded, rewarded)
-
-#         return answer
-
-        n = len(values)
         answer = 0
-        max_rewarded = values[0] + 0
-        for i in range(1, n):
-            v = values[i]
-            penalted = v - i
-            answer = max(answer, max_rewarded + penalted)
-            rewarded = v + i
-            max_rewarded = max(max_rewarded, rewarded)
+        max_rewarded = values[0]  # + 0 생략
+        for i in range(1, len(values)):
+            # max_rewarded에 penalted를 더한 것과 answer 비교
+            answer = max(answer, max_rewarded + values[i] - i)
+            # max_rewarded 갱신
+            max_rewarded = max(max_rewarded, values[i] + i)
         
         return answer
