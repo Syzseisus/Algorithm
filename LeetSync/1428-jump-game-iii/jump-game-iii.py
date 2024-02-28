984ec4921d2444f0ee6a6c1af4b181b6662bc707
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-02-28 11:44:09 +0900

    Time: 220 ms (78.94%) | Memory: 23.2 MB (84.25%) - LeetSync

--- LeetSync/1428-jump-game-iii/jump-game-iii.py
+++ LeetSync/1428-jump-game-iii/jump-game-iii.py
@@ -1,58 +1,54 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        N = len(arr)
-        
-        queue = [start]
-        # visited = set()
-        visited = 0
-
-        while queue:
-            curr_ind = queue.pop()
-            # visited.add(curr_ind)
-            visited |= (1 << curr_ind)
-
-            left_ind = curr_ind - arr[curr_ind]
-            if 0 <= left_ind < N:
-                if arr[left_ind] == 0:
-                    return True
-                # elif left_ind not in visited:
-                elif not visited & (1 << left_ind):
-                    queue.append(left_ind)
-
-            right_ind = curr_ind + arr[curr_ind]
-            if 0 <= right_ind < N:
-                if arr[right_ind] == 0:
-                    return True
-                # elif right_ind not in visited:
-                elif not visited & (1 << right_ind):
-                    queue.append(right_ind)
-        
-        return False
-        
-        
-        
 #         N = len(arr)
+        
+#         queue = [start]
+#         visited = set()
 
-#         def bfs(start_ind):
-#             queue = [start_ind]
-#             visited = set()
-#             while queue:
-#                 curr_ind = queue.pop()
-                
-#                 if curr_ind in visited:
-#                     continue
-#                 visited.add(curr_ind)
-                
-#                 if curr_ind < 0:
-#                     continue
-#                 elif curr_ind >= N:
-#                     continue
-#                 elif arr[curr_ind] == 0:
+#         while queue:
+#             curr_ind = queue.pop()
+#             visited.add(curr_ind)
+
+#             left_ind = curr_ind - arr[curr_ind]
+#             if 0 <= left_ind < N:
+#                 if arr[left_ind] == 0:
 #                     return True
+#                 elif left_ind not in visited:
+#                     queue.append(left_ind)
+
+#             right_ind = curr_ind + arr[curr_ind]
+#             if 0 <= right_ind < N:
+#                 if arr[right_ind] == 0:
+#                     return True
+#                 elif right_ind not in visited:
+#                     queue.append(right_ind)
+        
+#         return False
+        
+        
+        
+        N = len(arr)
+
+        def bfs(start_ind):
+            queue = [start_ind]
+            visited = set()
+            while queue:
+                curr_ind = queue.pop()
                 
-#                 queue.append(curr_ind + arr[curr_ind])
-#                 queue.append(curr_ind - arr[curr_ind])
+                if curr_ind in visited:
+                    continue
+                visited.add(curr_ind)
+                
+                if curr_ind < 0:
+                    continue
+                elif curr_ind >= N:
+                    continue
+                elif arr[curr_ind] == 0:
+                    return True
+                
+                queue.append(curr_ind + arr[curr_ind])
+                queue.append(curr_ind - arr[curr_ind])
 
-#             return False
+            return False
 
-#         return bfs(start)
+        return bfs(start)
