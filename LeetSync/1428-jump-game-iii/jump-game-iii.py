d3563d9fec0b164919466547f71dba9cb136ff41
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-02-28 10:51:27 +0900

    Time: 224 ms (65.41%) | Memory: 23.5 MB (71.98%) - LeetSync

---
+++ LeetSync/1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        N = len(arr)
+
+        def bfs(start_ind):
+            queue = deque([start_ind])
+            visited = set()
+            while queue:
+                curr_ind = queue.popleft()
+                
+                if curr_ind < 0:
+                    continue
+                elif curr_ind >= N:
+                    continue
+                elif arr[curr_ind] == 0:
+                    return True
+                
+                if curr_ind in visited:
+                    continue
+                visited.add(curr_ind)
+                
+                queue.append(curr_ind + arr[curr_ind])
+                queue.append(curr_ind - arr[curr_ind])
+
+            return False
+
+        return bfs(start)
