da167f4471054e5d2216db65ecd8ddc9e816f97a
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-02-28 11:42:09 +0900

    Time: 305 ms (9.25%) | Memory: 23.3 MB (84.25%) - LeetSync

--- LeetSync/1428-jump-game-iii/jump-game-iii.py
+++ LeetSync/1428-jump-game-iii/jump-game-iii.py
@@ -3,24 +3,28 @@ class Solution:
         N = len(arr)
         
         queue = [start]
-        visited = set()
+        # visited = set()
+        visited = 0
 
         while queue:
             curr_ind = queue.pop()
-            visited.add(curr_ind)
+            # visited.add(curr_ind)
+            visited |= (1 << curr_ind)
 
             left_ind = curr_ind - arr[curr_ind]
             if 0 <= left_ind < N:
                 if arr[left_ind] == 0:
                     return True
-                elif left_ind not in visited:
+                # elif left_ind not in visited:
+                elif not visited & (1 << left_ind):
                     queue.append(left_ind)
 
             right_ind = curr_ind + arr[curr_ind]
             if 0 <= right_ind < N:
                 if arr[right_ind] == 0:
                     return True
-                elif right_ind not in visited:
+                # elif right_ind not in visited:
+                elif not visited & (1 << right_ind):
                     queue.append(right_ind)
         
         return False
