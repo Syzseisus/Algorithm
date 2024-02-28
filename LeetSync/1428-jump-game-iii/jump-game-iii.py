c6ed848a00e523c04036844c794d5530cbfdfc4a
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-02-28 11:50:04 +0900

    Time: 219 ms (82.29%) | Memory: 23.2 MB (94.93%) - LeetSync

--- LeetSync/1428-jump-game-iii/jump-game-iii.py
+++ LeetSync/1428-jump-game-iii/jump-game-iii.py
@@ -1,5 +1,6 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
+#         # DFS
 #         N = len(arr)
         
 #         queue = [start]
@@ -9,13 +10,18 @@ class Solution:
 #             curr_ind = queue.pop()
 #             visited.add(curr_ind)
 
+#             # index 작아지는 방향
 #             left_ind = curr_ind - arr[curr_ind]
+#             # index 유효한 경우에만
 #             if 0 <= left_ind < N:
+#                 # value 0이면 return True 종료
 #                 if arr[left_ind] == 0:
 #                     return True
+#                 # visited에 없으면 queue에 추가
 #                 elif left_ind not in visited:
 #                     queue.append(left_ind)
 
+#             # 커지는 방향
 #             right_ind = curr_ind + arr[curr_ind]
 #             if 0 <= right_ind < N:
 #                 if arr[right_ind] == 0:
@@ -24,30 +30,28 @@ class Solution:
 #                     queue.append(right_ind)
         
 #         return False
-        
-        
-        
+
+        # BFS
         N = len(arr)
 
         def bfs(start_ind):
             queue = [start_ind]
             visited = set()
+
             while queue:
                 curr_ind = queue.pop()
                 
                 if curr_ind in visited:
                     continue
-                visited.add(curr_ind)
                 
-                if curr_ind < 0:
-                    continue
-                elif curr_ind >= N:
-                    continue
-                elif arr[curr_ind] == 0:
-                    return True
-                
-                queue.append(curr_ind + arr[curr_ind])
-                queue.append(curr_ind - arr[curr_ind])
+                if 0 <= curr_ind < N:
+                    visited.add(curr_ind)
+
+                    if arr[curr_ind] == 0:
+                        return True
+
+                    queue.append(curr_ind + arr[curr_ind])
+                    queue.append(curr_ind - arr[curr_ind])
 
             return False
 
