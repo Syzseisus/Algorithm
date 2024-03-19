5efc366cdfc044a65e069b63168f7c9d3e026902
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-03-19 11:37:46 +0900

    Time: 140 ms (5.12%) | Memory: 17.1 MB (88.37%) - LeetSync

--- LeetSync/1222-remove-covered-intervals/remove-covered-intervals.py
+++ LeetSync/1222-remove-covered-intervals/remove-covered-intervals.py
@@ -1,10 +1,22 @@
+# 결국 removed
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        answer = 0
-        end = 0
-        intervals.sort(key=lambda x: (x[0], -x[1]))
-        for _, e in intervals:
-            if e > end:
-                answer += 1
-                end = e
-        return answer
+        n = len(intervals)
+        
+        removed = [False] * n
+        
+        # arr2가 arr1을 cover하는지 체크
+        covered = lambda arr1, arr2: arr1[0] >= arr2[0] and arr1[1] <= arr2[1]
+        
+        # 모든 interval을 돌며 cover가 되는 쌍이 있는지 체크
+        for i in range(n):
+            for j in range(n):
+                if i == j or removed[j]:
+                    continue
+                
+                removed[i] ^= covered(intervals[i], intervals[j])  # <- 굳이?
+                
+                if removed[i]:
+                    break
+        
+        return n - sum(removed)
