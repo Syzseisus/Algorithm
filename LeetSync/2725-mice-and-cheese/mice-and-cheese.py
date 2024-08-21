72a3f3e61a192aa692008bfbe716d4179504b418
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-08-21 23:14:58 +0900

    Time: 639 ms (96.06%) | Memory: 30.1 MB (98.14%) - LeetSync

---
+++ LeetSync/2725-mice-and-cheese/mice-and-cheese.py
@@ -0,0 +1,19 @@
+import heapq as hq
+
+
+class Solution:
+    '''
+    i번째 치즈의 실제 reward는 reward1[i] - reward2[i]이다.
+    1번쥐가 k개 먹고 2번쥐가 나머지 모두를 먹는 게 조건이다.
+    따라서 반대로, 2번쥐가 전체를 먹고, (reward1[i] - reward2[i])이 큰 값,
+    즉 1번쥐가 먹는 게 더 이득인 치즈들을 k개 가져오면 된다.
+    '''
+    def miceAndCheese(self, reward1: List[int], reward2: List[int], k: int) -> int:
+        real_reward = [r2 - r1 for r1, r2 in zip(reward1, reward2)]
+        hq.heapify(real_reward)
+        answer = sum(reward2)
+        for _ in range(k):
+            a = hq.heappop(real_reward)
+            answer -= a
+        
+        return answer
