0d74bc913c78561559f3fce1bff337f75b7f392a
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-08-29 06:08:28 +0900

    Time: 59 ms (52.88%) | Memory: 18.8 MB (17.90%) - LeetSync

--- LeetSync/260-single-number-iii/single-number-iii.py
+++ LeetSync/260-single-number-iii/single-number-iii.py
@@ -1,3 +1,4 @@
+
 from collections import Counter
 
 class Solution:
@@ -5,9 +6,16 @@ class Solution:
         if len(nums) == 2:
             return nums
         
+        cnt = 0
         answer = []
+        # 이거 쓰면 iterable를 가지고 {요소: 개수} 형태의 `Counter` 객체로 만듦
         for k, v in Counter(nums).items():
+            # 한 개인 걸 answer에 넣기
             if v == 1:
                 answer.append(k)
+                cnt += 1
+            # 좀 더 빠르게 종료하기 위해 1개짜리 두 개되면 바로 종료
+            if cnt == 2:
+                return answer
         
         return answer
