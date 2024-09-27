c5dd6fe9ba294b3b51fec7968986c30d2a2fe71f
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-09-27 13:33:21 +0900

    Time: 40 ms (31.75%) | Memory: 16.6 MB (87.49%) - LeetSync

--- LeetSync/151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ LeetSync/151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,9 +1,26 @@
-# from collections import deque
+from collections import deque
 
 class Solution:
-    # 세번 평균
     def reverseWords(self, s: str) -> str:
-        # s.split() -> words를 word로 찢음
-        # [::-1] -> 순서 뒤집음
-        # ' '.join() -> word를 words로 합침
-        return ' '.join(s.split()[::-1])
+        # deque로 word 하나씩 stack 쌓기
+        words = deque([])
+        word = ''
+        for char in s:
+            # char가 공백이면
+            if char == ' ':
+                # word가 있을 경우 (공백이 여러 개 연속되는 경우를 skip하기 위함)
+                if word != '':
+                    # word를 stack하고 초기화
+                    words.appendleft(word)
+                    word = ''
+                continue
+            # char가 공백이 아니면 word 계속 늘리기
+            word += char
+
+        # for문 끝났는데 word가 남아 있으면 걔도 stack 하기
+        if word != '':
+            words.appendleft(word)
+        
+        # join해서 정답 words 생성
+        return ' '.join(words)
+            
