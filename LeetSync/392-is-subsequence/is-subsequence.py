6ba54f5bb4d2eb817d21ee7e415e101b797f120d
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-10-04 14:28:21 +0900

    Time: 25 ms (97.77%) | Memory: 16.8 MB (5.33%) - LeetSync

--- LeetSync/392-is-subsequence/is-subsequence.py
+++ LeetSync/392-is-subsequence/is-subsequence.py
@@ -1,13 +1,16 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        len_s = len(s)
+        len_t = len(t)
+        
         # 예외 1: s가 공백인 경우
         if not s:
             return True
         # 예외 2: s가 더 길 경우
-        if len(s) > len(t):
+        if len_s > len_t:
             return False
         # 예외 3: 길이가 1일 경우
-        if len(s) == len(t) == 1:
+        if len_s == len_t == 1:
             return s == t
 
         # for문은 t로 돌리고 s는 ind를 이용해서 돌림
@@ -17,9 +20,8 @@ class Solution:
             if mother == s[ind]:
                 ind += 1
             # 중간에 s가 전부 커버되면 성공
-            if ind == len(s):
+            if ind == len_s:
                 return True
         
         # for 문 다 돌았는데 커버 안되면 실패
-        # return ind == len(s)
         return False
