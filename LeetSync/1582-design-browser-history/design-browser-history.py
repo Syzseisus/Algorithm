11e1441dd15992c7eacf453ee5639adf582e067b
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2024-12-02 22:48:33 +0900

    Time: 26 ms (83.36%) | Memory: 19.8 MB (12.75%) - LeetSync

--- LeetSync/1582-design-browser-history/design-browser-history.py
+++ LeetSync/1582-design-browser-history/design-browser-history.py
@@ -1,56 +1,32 @@
-'''
-Input:
-["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
-[["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
-Output:
-[null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
-'''
-
 class BrowserHistory:
+    """
+    self.history : 홈페이지를 방문한 순으로 기록 (`{index: url}`)
+    self.curr_pg : 현재 있는 홈페이지 인덱스
+    self.max_num : history에 있는 최대 페이지 개수
+    """
 
     def __init__(self, homepage: str):
+        """`0`번 인덱스에 인자로 입력된 `homepage` 저장"""
         self.history = {0: homepage}
         self.curr_pg = 0
         self.max_num = 0
 
     def visit(self, url: str) -> None:
-        # print("current page:", self.curr_pg)
-        # print("current state:", self.history)
+        """
+        인덱스와 페이지 개수 하나 늘리고, `self.history` 업데이트
+        만약 `visit` 메서드 호출 전에 `self.curr_pg`가 `self.max_num`보다 작다면
+            -> `self.history` 덮어쓰기 + 이후 기록 접근 불가 (`self.max_num` 초기화)
+        """
         self.curr_pg += 1
         self.max_num = self.curr_pg
         self.history[self.curr_pg] = url
-        # print("updated page:", self.curr_pg)
-        # print("updated state:", self.history)
-        # print()
 
     def back(self, steps: int) -> str:
-        # print("current page:", self.curr_pg)
-        # print("current state:", self.history)
-        self.curr_pg -= steps
-        if self.curr_pg < 0:
-            self.curr_pg = 0
-        # print("updated page:", self.curr_pg)
-        # print("updated state:", self.history)
-        # print()
+        # step만큼 이동하지만 0보다는 작아질 순 없음
+        self.curr_pg = max(self.curr_pg - steps, 0)
         return self.history[self.curr_pg]
 
     def forward(self, steps: int) -> str:
-        # print("current page:", self.curr_pg)
-        # print("current state:", self.history)
-        next_pg = self.curr_pg + steps
-        if next_pg <= self.max_num:
-            self.curr_pg = next_pg
-        else:
-            self.curr_pg = self.max_num
-        # print("updated page:", self.curr_pg)
-        # print("updated state:", self.history)
-        # print()
+        # step만큼 이동하지만 `self.max_num`보다는 작아질 순 없음
+        self.curr_pg = min(self.curr_pg + steps, self.max_num)
         return self.history[self.curr_pg]
-
-
-
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
