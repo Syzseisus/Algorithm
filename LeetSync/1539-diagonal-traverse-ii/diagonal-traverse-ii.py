8fcc8d1c355e63de13315be347bfb28e0fed4c74
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-02-06 16:50:19 +0900

    Time: 93 ms (44.21%) | Memory: 42.3 MB (49.15%) - LeetSync

--- LeetSync/1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ LeetSync/1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -1,40 +1,18 @@
-# class Solution:
-#     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-#         col = 0
-#         for num in nums:
-#             col = max(len(num), col)
-
-#         answer = []
-#         for d in range(len(nums) + col - 1):
-#             temp = []
-#             for i, row in enumerate(nums):
-#                 if i > d:
-#                     continue
-#                 try:
-#                     temp.append(row[d - i])
-#                 except:
-#                     continue
-#             temp = temp[::-1]
-#             answer.extend(temp)
-#         return answer
-
-
-from collections import defaultdict
-from typing import List
+from collections import defaultdict as ddict
 
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        diagonal_map = defaultdict(list)
-        
-        # 각 (i, j) 위치의 요소를 i + j를 key로 하여 저장
+        diag = ddict(list)
+
+        # 대각선마다 저장
         for i, row in enumerate(nums):
             for j, val in enumerate(row):
-                diagonal_map[i + j].append(val)
+                diag[i + j].append(val)
         
         answer = []
-        
-        # 대각선 순서대로 리스트를 병합
-        for key in sorted(diagonal_map.keys()):  
-            answer.extend(reversed(diagonal_map[key]))  # 역순 추가
+        # 대각선 순서대로 리스트를 병합하는데
+        for key in sorted(diag.keys()):
+            # 뒤집어야됨
+            answer.extend(diag[key][::-1])
         
         return answer
