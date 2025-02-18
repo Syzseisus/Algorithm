1a24318116c280b0bc95a363362212f023d15481
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-02-18 15:30:43 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.26%) - LeetSync

--- LeetSync/2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ LeetSync/2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -1,24 +1,44 @@
-from collections import Counter
+from collections import Counter, deque
+
+# class Solution:
+#     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+#         # 가능한 의자 찾기
+#         seats = Counter(seats)
+
+#         # 학생 정렬하고 탐색
+#         students.sort()
+#         answer = 0
+#         for s in students:
+#             # 자기와 가장 가까운 
+#             min_pos = min(seats)
+#             if (s in seats) and (seats[s] > 0):
+#                 seats[s] -= 1
+#                 if seats[s] == 0:
+#                     del seats[s]
+#                     if not seats:
+#                         break
+#             else:
+#                 answer += abs(min_pos - s)
+#                 seats[min_pos] -= 1
+#                 if seats[min_pos] == 0:
+#                     del seats[min_pos]
+#                     if not seats:
+#                         break
+
+#         return answer
+
 
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats = Counter(seats)
+        # 의자 정렬
+        seats.sort()
+        seats = deque(seats)
+
+        # 학생 정렬하고 탐색
         students.sort()
         answer = 0
         for s in students:
-            min_pos = min(seats)
-            if (s in seats) and (seats[s] > 0):
-                seats[s] -= 1
-                if seats[s] == 0:
-                    del seats[s]
-                    if not seats:
-                        break
-            else:
-                answer += abs(min_pos - s)
-                seats[min_pos] -= 1
-                if seats[min_pos] == 0:
-                    del seats[min_pos]
-                    if not seats:
-                        break
+            curr_seat = seats.popleft()
+            answer += abs(curr_seat - s)
 
         return answer
