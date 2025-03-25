39be49688e5b6ff2bf446a7a58a386456a2a336e
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-03-25 17:33:39 +0900

    Time: 3 ms (32.80%) | Memory: 17.9 MB (39.18%) - LeetSync

--- LeetSync/726-number-of-atoms/number-of-atoms.py
+++ LeetSync/726-number-of-atoms/number-of-atoms.py
@@ -41,3 +41,47 @@ class Solution:
 
         sorted_tmp = sorted([f"{atom}" if count == 1 else f"{atom}{count}" for atom, count in stack[-1].items()])
         return "".join(sorted_tmp)
+
+
+# solution...
+
+from collections import Counter
+
+class Solution:
+    def get_count(self, s: str, idx: int) -> str:
+        count = ""
+        while idx < len(s) and s[idx].isdigit():
+            count += s[idx]
+            idx += 1
+
+        count = int(count) if count else 1
+        return count, idx
+
+    def countOfAtoms(self, formula: str) -> str:
+        index = 0
+        stack = [Counter()]
+
+        while index < len(formula):
+            s = formula[index]
+            if s == "(":
+                stack.append(Counter())
+                index += 1
+            elif s == ")":
+                count, index = self.get_count(formula, index + 1)
+                curr_tmp = stack.pop()
+                curr_tmp = Counter({k: v * count for k, v in curr_tmp.items()})
+                prev_tmp = stack[-1]
+                stack[-1] = curr_tmp + prev_tmp
+            elif s.isupper():
+                index += 1
+                atom = s
+                while index < len(formula) and formula[index].islower():
+                    atom += formula[index]
+                    index += 1
+                
+                count, index = self.get_count(formula, index)
+                prev_tmp = stack[-1]
+                prev_tmp[atom] += count
+
+        sorted_tmp = sorted([f"{atom}" if count == 1 else f"{atom}{count}" for atom, count in stack[-1].items()])
+        return "".join(sorted_tmp)
