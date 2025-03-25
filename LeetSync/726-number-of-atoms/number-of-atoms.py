2a6120d85660a33d03b7173fa394e4dd4ccbba04
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-03-25 17:34:32 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.13%) - LeetSync

--- LeetSync/726-number-of-atoms/number-of-atoms.py
+++ LeetSync/726-number-of-atoms/number-of-atoms.py
@@ -1,46 +1,46 @@
-# solution...
+# # solution...
 
-from collections import defaultdict as ddict
+# from collections import defaultdict as ddict
 
-class Solution:
-    def get_count(self, s: str, idx: int) -> str:
-        count = ""
-        while idx < len(s) and s[idx].isdigit():
-            count += s[idx]
-            idx += 1
+# class Solution:
+#     def get_count(self, s: str, idx: int) -> str:
+#         count = ""
+#         while idx < len(s) and s[idx].isdigit():
+#             count += s[idx]
+#             idx += 1
 
-        count = int(count) if count else 1
-        return count, idx
+#         count = int(count) if count else 1
+#         return count, idx
 
-    def countOfAtoms(self, formula: str) -> str:
-        index = 0
-        stack = [ddict(int)]
+#     def countOfAtoms(self, formula: str) -> str:
+#         index = 0
+#         stack = [ddict(int)]
 
-        while index < len(formula):
-            s = formula[index]
-            if s == "(":
-                stack.append(ddict(int))
-                index += 1
-            elif s == ")":
-                count, index = self.get_count(formula, index + 1)
-                curr_tmp = stack.pop()
-                curr_tmp = {k: v * count for k, v in curr_tmp.items()}
-                prev_tmp = stack[-1]
-                for k, v in curr_tmp.items():
-                    prev_tmp[k] += v
-            elif s.isupper():
-                index += 1
-                atom = s
-                while index < len(formula) and formula[index].islower():
-                    atom += formula[index]
-                    index += 1
+#         while index < len(formula):
+#             s = formula[index]
+#             if s == "(":
+#                 stack.append(ddict(int))
+#                 index += 1
+#             elif s == ")":
+#                 count, index = self.get_count(formula, index + 1)
+#                 curr_tmp = stack.pop()
+#                 curr_tmp = {k: v * count for k, v in curr_tmp.items()}
+#                 prev_tmp = stack[-1]
+#                 for k, v in curr_tmp.items():
+#                     prev_tmp[k] += v
+#             elif s.isupper():
+#                 index += 1
+#                 atom = s
+#                 while index < len(formula) and formula[index].islower():
+#                     atom += formula[index]
+#                     index += 1
                 
-                count, index = self.get_count(formula, index)
-                prev_tmp = stack[-1]
-                prev_tmp[atom] += count
+#                 count, index = self.get_count(formula, index)
+#                 prev_tmp = stack[-1]
+#                 prev_tmp[atom] += count
 
-        sorted_tmp = sorted([f"{atom}" if count == 1 else f"{atom}{count}" for atom, count in stack[-1].items()])
-        return "".join(sorted_tmp)
+#         sorted_tmp = sorted([f"{atom}" if count == 1 else f"{atom}{count}" for atom, count in stack[-1].items()])
+#         return "".join(sorted_tmp)
 
 
 # solution...
