e60d3ba2582b74f4a1f2d0a757664ea96766db97
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-03-31 15:35:34 +0900

    Time: 1 ms (93.92%) | Memory: 19.5 MB (16.10%) - LeetSync

--- LeetSync/547-number-of-provinces/number-of-provinces.py
+++ LeetSync/547-number-of-provinces/number-of-provinces.py
@@ -2,22 +2,18 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         # dfs 함수 - 함수 내부에 정의해서 로컬 변수 같이 쓰기
         def dfs(curr):
+            # 현재 노드 방문 처리
             visited[curr] = True
             for ngbr in range(n):
+                # 연결되어 있고 방문한 노드가 아니라면 dfs 재귀
                 if isConnected[curr][ngbr] and not visited[ngbr]:
-                    # visited[ngbr] = True
                     dfs(ngbr)
-
-                if not visited[i]:
-                    dfs(i, visited)
-
 
         n = len(isConnected)
         visited = [False for _ in range(n)]
         answer = 0
         for i in range(n):
             if not visited[i]:
-                # visited[i] = True
                 answer += 1
                 dfs(i)
         
