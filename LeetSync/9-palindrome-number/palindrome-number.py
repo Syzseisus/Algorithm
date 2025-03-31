8d5d1061a0fde962f28539f6f30f69aa121282fd
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-03-31 13:05:06 +0900

    Time: 11 ms (36.38%) | Memory: 17.6 MB (98.30%) - LeetSync

--- LeetSync/9-palindrome-number/palindrome-number.py
+++ LeetSync/9-palindrome-number/palindrome-number.py
@@ -1,12 +1,13 @@
 class Solution:
+    # 풀이 1: 정석
     def isPalindrome(self, x: int) -> bool:
-        # if x < 0:
-        #     return False
-        # elif x == 0:
-        #     return True
+        # 예외처리 1: 음수는 무조건 안 됨
+        if x < 0:
+            return False
+        # 예외처리 2: 한 자리는 무조건 됨
+        elif x < 10:
+            return True
 
-        # row = str(x)
-        # col = row[::-1]
-        
-        # return row == col
-        return str(x) == str(x)[::-1]
+        row = str(x)       # string 변환하고
+        pal = row[::-1]    # 뒤집어서
+        return row == pal  # 같은지 확인
