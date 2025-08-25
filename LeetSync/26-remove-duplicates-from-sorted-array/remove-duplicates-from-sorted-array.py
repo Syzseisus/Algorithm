f50063f56a9bbf3b11bfa7f3024e13f95e8ddd62
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2025-08-25 13:15:17 +0900

    Time: 1 ms (60.16%) | Memory: 19 MB (48.19%) - LeetSync

--- LeetSync/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ LeetSync/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,22 +1,10 @@
-from collections import defaultdict as ddict
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        helper = ddict(int)
-        for n in nums:
-            helper[n] += 1
-        # print(helper)
-        k = len(helper)
-        
-        n = -100
-        i = 0
-        while i < k:
-            # print('ㅁ', i, n)
-            if n in helper:
-                # print("H")
-                nums[i] = n
-                i += 1
-            n += 1
-        print(nums)
-        for i in range(k, len(nums)):
-            nums[i] = "_"
+        k = 1  # 첫 번째 값은 항상 고유함
+        for i in range(1, len(nums)):
+            # i번째에 새로운 게 나온다면
+            # ps. k-1인 건 인덱스가 0부터 시작하기 때문
+            if nums[i] != nums[k - 1]:
+                nums[k] = nums[i]  # 다음 위치 (k-1 + 1)에 갖고오고
+                k += 1  # k 증가
         return k
